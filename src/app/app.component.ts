b29feb9212b2561db2d80ad0b85570c694ebd25e
Михаил Грачев <mikhail.grachev@eventplatform.ru>
2021-08-29 13:25:56 +0300

    NGRX-1 добавлен код счетчика

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +6,25 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'ngrx';
+  public counter = 0;
+  public updatedAt?: number;
+
+  get cannotDecrease(): boolean {
+    return this.counter <= 0;
+  }
+
+  increase(): void {
+    this.updatedAt = Date.now();
+    this.counter++;
+  }
+
+  decrease(): void {
+    this.updatedAt = Date.now();
+    this.counter--;
+  }
+
+  clear(): void {
+    this.updatedAt = Date.now();
+    this.counter = 0;
+  }
 }
