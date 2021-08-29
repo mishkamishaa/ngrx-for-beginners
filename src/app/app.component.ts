f5b7c33353a37f6ca0c6c03497b492f1fe7cdf80
Михаил Грачев <mikhail.grachev@eventplatform.ru>
2021-08-29 15:40:55 +0300

    NGRX-4 добавлен эффект для обновления времени

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,6 @@
 import {Component} from '@angular/core';
 import {Store} from "@ngrx/store";
-import {clear, countSelector, decrease, increase} from "./reducers/counter";
+import {clear, countSelector, decrease, increase, updatedAtSelector} from "./reducers/counter";
 import {map} from "rxjs/operators";
 
 @Component({
@@ -9,25 +9,22 @@ import {map} from "rxjs/operators";
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  public updatedAt?: number;
   public count$ = this.store.select(countSelector)
   public cannotDecrease$ = this.count$.pipe(map(count => count <= 0));
+  public updatedAt$ = this.store.select(updatedAtSelector);
 
   constructor(private store: Store) {
   }
 
   increase(): void {
     this.store.dispatch(increase());
-    this.updatedAt = Date.now();
   }
 
   decrease(): void {
     this.store.dispatch(decrease());
-    this.updatedAt = Date.now();
   }
 
   clear(): void {
     this.store.dispatch(clear());
-    this.updatedAt = Date.now();
   }
 }
